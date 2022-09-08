0131fa78e3cba34f30f64e307219025748e0d350
Dmitry Soloma <flyingmeatsoul@gmail.com>
2022-09-08 18:07:18 +0300

    add example route hook

---
+++ example/gob_duplex_route_hook/server.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"github.com/dimonrus/gocli"
+	"github.com/dimonrus/tcpless"
+	"sync"
+)
+
+// StartServer start server
+func StartServer(config *tcpless.Config, application gocli.Application) *tcpless.Server {
+	server := tcpless.NewServer(config, Handler(nil), NewHelloClient, App.GetLogger())
+	err := server.Start()
+	if err != nil {
+		application.FatalError(err)
+	}
+	go resetRps()
+	return server
+}
+
+// StartClient start client and make call
+func StartClient(config *tcpless.Config, app gocli.Application) {
+	requests := 10
+	parallel := 2
+
+	wg := sync.WaitGroup{}
+	wg.Add(parallel)
+	for i := 0; i < parallel; i++ {
+		go func() {
+			defer wg.Done()
+			client := NewHelloClient(config, app.GetLogger()).(*HelloClient)
+			_, err := client.Dial()
+			if err != nil {
+				app.FailMessage(err.Error())
+				return
+			}
+			for j := 0; j < requests; j++ {
+				err = client.Hello(getTestUser())
+				if err != nil {
+					app.FailMessage(err.Error())
+					return
+				}
+			}
+			_ = client.Stream().Release()
+		}()
+	}
+	wg.Wait()
+}
